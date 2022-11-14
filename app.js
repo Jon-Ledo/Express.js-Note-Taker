47873a42353cdfd2b80949464fb88eaf43f088af
JonLedo <jonledo.code@gmail.com>
2022-11-14 11:40:10 -0500

    app successfully gets note data, and saves data from POSTs

--- app.js
+++ app.js
@@ -1,12 +1,82 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const path = require('path')
+const port = 5000
+const { readFile, writeFile } = require('fs')
+const noteData = require('./db/db.json')
 
-app.use(express.static('./public'))
+// middlewares
+app.use(express.static('public'))
 app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
 
 app.get('/notes', (req, res) => {
-  console.log('connected')
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+// GET request for notes
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(noteData)
+
+  console.info(`${req.method} request received to get notes`)
+})
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // const data = [{ name: 'Jon', Age: 32 }]
+  // writeFile('./db/db.json', data, () => {
+  //   console.log('testing 123')
+  // })
+  console.info(`${req.method} request received to add a note`)
+
+  const { title, text } = req.body
+
+  if (title && text) {
+    const id = new Date().getMilliseconds()
+    const newNote = {
+      title,
+      text,
+      id,
+    }
+
+    readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+      } else {
+        // convert data into JSON
+        const parsedData = JSON.parse(data)
+
+        // push new note into previous data array
+        parsedData.push(newNote)
+
+        writeFile(
+          './db/db.json',
+          JSON.stringify(parsedData, null, 4),
+          (err) => {
+            if (err) {
+              console.error(err)
+            } else {
+              console.info('Updated notes')
+            }
+          }
+        )
+      }
+    })
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    }
+
+    console.log(response)
+    res.status(201).json(response)
+  } else {
+    res.status(500).json('Error in posting note')
+  }
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.listen(port, () => {
