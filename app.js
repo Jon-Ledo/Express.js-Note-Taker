0efa489138cf7eff2b4cbfcd1aea7d4e4f9860db
JonLedo <jonledo.code@gmail.com>
2022-11-13 22:34:04 -0500

    added middleware, .listen and one route

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.static('./public'))
+app.use(express.json())
+
+app.get('/notes', (req, res) => {
+  console.log('connected')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`)
+})
