a3031382988261118e316b109790f89ca37b3d29
JonLedo <jonledo.code@gmail.com>
2022-11-14 18:18:02 -0500

    created a routes folder and used express.Router() to make the ap.js look cleaner. changed imports where necessary and moved functions around.

--- app.js
+++ app.js
@@ -1,112 +1,16 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 const port = process.env.PORT || 3001
-const { readFile, writeFile } = require('fs')
-const noteData = require('./db/db.json')
-const { v4: uuidv4 } = require('uuid')
+const htmlRoute = require('./routes/html')
+const apiRoute = require('./routes/api')
 
 // middlewares
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
-
-// GET request for notes
-app.get('/api/notes', (req, res) => {
-  res.status(200).json(noteData)
-
-  console.info(`${req.method} request received to get notes`)
-})
-
-// POST request to add a note
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note`)
-
-  const { title, text } = req.body
-
-  if (title && text) {
-    const id = uuidv4()
-    const newNote = {
-      title,
-      text,
-      id,
-    }
-
-    readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err)
-      } else {
-        // convert data into JSON
-        const parsedData = JSON.parse(data)
-
-        // push new note into previous data array
-        parsedData.push(newNote)
-
-        writeFile(
-          './db/db.json',
-          JSON.stringify(parsedData, null, 4),
-          (err) => {
-            if (err) {
-              console.error(err)
-            } else {
-              console.info('Updated notes')
-            }
-          }
-        )
-      }
-    })
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    }
-
-    console.log(response)
-    res.status(201).json(response)
-  } else {
-    res.status(500).json('Error in posting note')
-  }
-})
-
-// DELETE target note
-app.delete('/api/notes/:id', (req, res) => {
-  const { id } = req.params
-
-  readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err)
-    } else {
-      // convert data into JSON
-      const parsedData = JSON.parse(data)
-
-      // use note ID to find index in db.json
-      const index = noteData.findIndex((note) => {
-        return note.id === id
-      })
-
-      // splice method to delete note
-      const removed = parsedData.splice(index, 1)
-
-      // write db.json with new changes
-      writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) => {
-        if (err) {
-          console.error(err)
-        } else {
-          console.info('Deleted Note')
-        }
-      })
-    }
-  })
-
-  res.status(200).json(noteData)
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+app.use('/api/notes', apiRoute)
+app.use('/', htmlRoute)
 
 app.listen(port, () => {
   console.log(`listening on port ${port}...`)
---
+++ routes/api.js
@@ -0,0 +1,96 @@
+const router = require('express').Router()
+const { readFile, writeFile } = require('fs')
+const noteData = require('../db/db.json')
+const { v4: uuidv4 } = require('uuid')
+
+// GET request for notes
+router.get('/', (req, res) => {
+  res.status(200).json(noteData)
+
+  console.info(`${req.method} request received to get notes`)
+})
+
+// POST request to add a note
+router.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`)
+
+  const { title, text } = req.body
+
+  if (title && text) {
+    const id = uuidv4()
+    const newNote = {
+      title,
+      text,
+      id,
+    }
+
+    readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+      } else {
+        // convert data into JSON
+        const parsedData = JSON.parse(data)
+
+        // push new note into previous data array
+        parsedData.push(newNote)
+
+        writeFile(
+          './db/db.json',
+          JSON.stringify(parsedData, null, 4),
+          (err) => {
+            if (err) {
+              console.error(err)
+            } else {
+              console.info('Updated notes')
+            }
+          }
+        )
+      }
+    })
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    }
+
+    console.log(response)
+    res.status(201).json(response)
+  } else {
+    res.status(500).json('Error in posting note')
+  }
+})
+
+// DELETE target note
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+    } else {
+      // convert data into JSON
+      const parsedData = JSON.parse(data)
+
+      // use note ID to find index in db.json
+      const index = noteData.findIndex((note) => {
+        return note.id === id
+      })
+
+      // splice method to delete note
+      const removed = parsedData.splice(index, 1)
+
+      // write db.json with new changes
+      writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) => {
+        if (err) {
+          console.error(err)
+        } else {
+          console.info('Deleted Note')
+        }
+      })
+    }
+  })
+
+  res.status(200).json(noteData)
+})
+
+module.exports = router
